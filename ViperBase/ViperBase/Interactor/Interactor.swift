80011a46be7e24fbb9e4cd90da2560a07f51f882
SOL <sol.bsuir@gmail.com>
2017-05-20 23:29:12 +0300

    Interactor documentation added

--- ViperBase/ViperBase/Interactor/Interactor.swift
+++ ViperBase/ViperBase/Interactor/Interactor.swift
@@ -8,6 +8,15 @@
 
 import UIKit
 
+/**
+ Base class for all business logic in VIPER arhitecture. Split your application business sogic to separate
+ *use cases* and put each use case to single interactor. Try to follow 'Single Responsibility' design principle
+ during develping you business logic (interactors) layer.
+ Your custom interactors may contain strong references to:
+ - Data managers (or other instances) that can load and store business models
+ - Data sessions - objects that hold mutable application state
+ - Other interactors (if it really nesessary)
+ */
 open class Interactor: NSObject, InteractorInterface {
     // Empty
 }
--- ViperBase/ViperBase/Interactor/InteractorInterface.swift
+++ ViperBase/ViperBase/Interactor/InteractorInterface.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+/**
+ Base interface for you business logic classes. Represents Presenter -> Interactor bridge: use only methods
+ presented in InteractorInterface child protocols to make calls from presenter.
+ Try to follow 'Single Responsibility' design principle during develping you business logic (interactors) layer
+ */
 public protocol InteractorInterface: NSObjectProtocol {
     // Empty
 }
